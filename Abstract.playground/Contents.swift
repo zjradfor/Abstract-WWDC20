a65b61ccc7850d41ca573e851bd1f4fe5e89c736
zjradfor <zjradfor@uwaterloo.ca>
2020-05-14 22:01:02 -0400

    add menu and some shape functionality

--- Abstract.playground/Contents.swift
+++ Abstract.playground/Contents.swift
@@ -3,18 +3,29 @@
 import UIKit
 import PlaygroundSupport
 
+/* TODO
+ - remove gesture when in resize state
+ - figure out which actions can carry into multiple states
+ - different shapes
+ - add random tag to shapes to find them later (delete)
+ - pictures for shape add buttons, position of where shapes spawn
+ - colour picker
+ - welcome view (help)
+ - image filter (done)
+ */
+
 class CanvasViewController : UIViewController {
     
     var canvas: UIView!
     var stackMenu: UIStackView!
     var helpButton: UIButton!
+    var addMenu: UIStackView!
     
     override func loadView() {
         canvas = UIView()
         
         canvas.backgroundColor = .white
 
-        
         configureUI()
         
         self.view = canvas
@@ -26,6 +37,7 @@ class CanvasViewController : UIViewController {
         setupNavBar()
         setupToolBar()
         setupHelpButton()
+        setupAddMenu()
     }
     
     private func setupNavBar() {
@@ -47,9 +59,7 @@ class CanvasViewController : UIViewController {
     }
     
     private func setupToolBar() {
-        stackMenu = UIStackView()
-        stackMenu.axis = .horizontal
-        stackMenu.frame = CGRect(x: 20, y: 50, width: 30, height: 50)
+        stackMenu = UIStackView(frame: CGRect(x: 20, y: 50, width: 30, height: 50))
         stackMenu.distribution = .fillProportionally
         stackMenu.spacing = 2.0
 
@@ -59,48 +69,48 @@ class CanvasViewController : UIViewController {
         let toolButton = UIButton(frame: commonButtonSize)
         toolButton.setImage(UIImage(systemName: "chevron.right", withConfiguration: symbolConfig), for: .normal)
         toolButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        toolButton.tag = 0
+        toolButton.tag = Tool.none.rawValue
         stackMenu.addArrangedSubview(toolButton)
         
         let addButton = UIButton(frame: commonButtonSize)
         addButton.setImage(UIImage(systemName: "plus", withConfiguration: symbolConfig), for: .normal)
         addButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        addButton.tag = 1
+        addButton.tag = Tool.add.rawValue
         addButton.isHidden = true
         stackMenu.addArrangedSubview(addButton)
 
         let colourButton = UIButton(frame: commonButtonSize)
         colourButton.setImage(UIImage(systemName: "paintbrush", withConfiguration: symbolConfig), for: .normal)
         colourButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        colourButton.tag = 2
+        colourButton.tag = Tool.colour.rawValue
         colourButton.isHidden = true
         stackMenu.addArrangedSubview(colourButton)
 
         let moveButton = UIButton(frame: commonButtonSize)
         moveButton.setImage(UIImage(systemName: "hand.draw", withConfiguration: symbolConfig), for: .normal)
         moveButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        moveButton.tag = 3
+        moveButton.tag = Tool.move.rawValue
         moveButton.isHidden = true
         stackMenu.addArrangedSubview(moveButton)
 
         let resizeButton = UIButton(frame: commonButtonSize)
         resizeButton.setImage(UIImage(systemName: "arrow.up.left.and.arrow.down.right", withConfiguration: symbolConfig), for: .normal)
         resizeButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        resizeButton.tag = 4
+        resizeButton.tag = Tool.resize.rawValue
         resizeButton.isHidden = true
         stackMenu.addArrangedSubview(resizeButton)
 
         let rotateButton = UIButton(frame: commonButtonSize)
         rotateButton.setImage(UIImage(systemName: "arrow.2.circlepath", withConfiguration: symbolConfig), for: .normal)
         rotateButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        rotateButton.tag = 5
+        rotateButton.tag = Tool.rotate.rawValue
         rotateButton.isHidden = true
         stackMenu.addArrangedSubview(rotateButton)
 
         let trashButton = UIButton(frame: commonButtonSize)
         trashButton.setImage(UIImage(systemName: "trash", withConfiguration: symbolConfig), for: .normal)
         trashButton.addTarget(self, action: #selector(handleToolBarEvent), for: .touchUpInside)
-        trashButton.tag = 6
+        trashButton.tag = Tool.trash.rawValue
         trashButton.isHidden = true
         stackMenu.addArrangedSubview(trashButton)
         
@@ -116,11 +126,41 @@ class CanvasViewController : UIViewController {
         canvas.addSubview(helpButton)
     }
     
+    private func setupAddMenu() {
+        addMenu = UIStackView(frame: CGRect(x: 0, y: 550, width: 380, height: 150))
+        addMenu.distribution = .fillEqually
+        
+        let commonButtonSize = CGRect(x: 0, y: 0, width: 150, height: 150)
+        
+        let squareButton = UIButton(frame: commonButtonSize)
+        squareButton.backgroundColor = .red
+        squareButton.layer.borderColor = UIColor.black.cgColor
+        squareButton.layer.borderWidth = 2
+        squareButton.addTarget(self, action: #selector(addShapePressed), for: .touchDown)
+        addMenu.addArrangedSubview(squareButton)
+        
+        let circleButton = UIButton(frame: commonButtonSize)
+        circleButton.backgroundColor = .blue
+        circleButton.layer.borderColor = UIColor.black.cgColor
+        circleButton.layer.borderWidth = 2
+        addMenu.addArrangedSubview(circleButton)
+        
+        let triangleButton = UIButton(frame: commonButtonSize)
+        triangleButton.backgroundColor = .yellow
+        triangleButton.layer.borderColor = UIColor.black.cgColor
+        triangleButton.layer.borderWidth = 2
+        addMenu.addArrangedSubview(triangleButton)
+        
+        addMenu.isHidden = true
+        
+        canvas.addSubview(addMenu)
+    }
+    
     //MARK: - Actions
     
     private func toggleToolBar() {
         UIView.animate(
-            withDuration: 0.8,
+            withDuration: 0.6,
             delay: 0,
             usingSpringWithDamping: 0.8,
             initialSpringVelocity: 0.2,
@@ -134,6 +174,18 @@ class CanvasViewController : UIViewController {
                 self.stackMenu.frame = CGRect(x: 20, y: 50, width: desiredWidth, height: 50)
                 (self.stackMenu.arrangedSubviews.first as! UIButton).setImage(UIImage(systemName: desiredImage, withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .semibold, scale: .medium)), for: .normal)
         }, completion: nil)
+    }
+    
+    private func toggleAddMenu() {
+        print("toggle add menu")
+        addMenu.isHidden = !addMenu.isHidden
+        if addMenu.isHidden {
+            stackMenu.arrangedSubviews[Tool.add.rawValue].tintColor = .systemBlue
+        }
+    }
+    
+    private func showColourPicker() {
+        print("show colour picker")
     }
     
     // MARK: - Event Handlers
@@ -153,38 +205,24 @@ class CanvasViewController : UIViewController {
         let buttonIndex = sender.tag
         let selectedButton = stackMenu.arrangedSubviews[buttonIndex] as! UIButton
         
+        guard buttonIndex != Tool.none.rawValue else {
+            toggleToolBar()
+            return
+        }
+        
         for button in stackMenu.arrangedSubviews where button != selectedButton {
             button.tintColor = .systemBlue
         }
         
-        if buttonIndex == 0 {
-            // tool button action
-            toggleToolBar()
-        } else if buttonIndex == 1 {
-            // add button action
-            
-        } else if buttonIndex == 2 {
-            // colour button action
-            selectedButton.tintColor = .black
-            
-        } else if buttonIndex == 3 {
-            // move button action
-            selectedButton.tintColor = .black
-            
-        } else if buttonIndex == 4 {
-            // resize button action
-            selectedButton.tintColor = .black
-            
-        } else if buttonIndex == 5 {
-            // rotate button action
-            selectedButton.tintColor = .black
-            
-        } else if buttonIndex == 6 {
-            // trash button action
-            selectedButton.tintColor = .black
-            
-        }
+        selectedButton.tintColor = .black
         
+        State.current = Tool(rawValue: buttonIndex) ?? .none
+        
+        if buttonIndex == Tool.add.rawValue {
+            toggleAddMenu()
+        } else if buttonIndex == Tool.colour.rawValue {
+            showColourPicker()
+        }
     }
     
     @objc
@@ -192,32 +230,57 @@ class CanvasViewController : UIViewController {
         print("help")
     }
     
+    @objc
+    func addShapePressed(_ sender: UIButton) {
+        // change button colour back to blue
+        // put menu down
+        // add shape on screen
+        let shape = Shape(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        canvas.addSubview(shape)
+
+        print("SHAPE")
+        canvas.bringSubviewToFront(stackMenu)
+    }
+    
     
 }
 
-enum EditState {
-    case colour
-    case move
-    case resize
-    case rotate
-    case delete
+class State {
+    static var current: Tool = .none
 }
 
-enum ShapeType {
-    case square
-    case triangle
-    case circle
+enum Tool: Int {
+    case none = 0
+    case add = 1
+    case colour = 2
+    case move = 3
+    case resize = 4
+    case rotate = 5
+    case trash = 6
+}
+
+enum ShapeType: Int {
+    case square = 0
+    case triangle = 1
+    case circle = 2
 }
 
 class Shape: UIView {
     
     var lastLocation: CGPoint = CGPoint.zero
+    var rotation: CGFloat = 0.0
+    var startRotationAngle: CGFloat = 0
+    let rotateAnimation = CABasicAnimation()
+    var proxy: CGFloat = 30
+    var touchStart = CGPoint.zero
+    var edgeTouched: Edge = .none
+    var panRecognizer: UIPanGestureRecognizer!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        let panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(detectPan))
-        gestureRecognizers = [panRecognizer]
+        panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(detectPan))
+        gestureRecognizers = [panRecognizer] // REMOVE GESURE WHEN RESIZING!
 
         backgroundColor = .red
     }
@@ -226,58 +289,12 @@ class Shape: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    @objc func detectPan(_ recognizer:UIPanGestureRecognizer) {
-        let translation  = recognizer.translation(in: self.superview)
-        self.center = CGPoint(x: lastLocation.x + translation.x, y: lastLocation.y + translation.y)
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.superview?.bringSubviewToFront(self)
+        //self.superview?.bringSubviewToFront(self)
         lastLocation = self.center
-    }
-    
-}
-
-
-class ResizableView: UIView {
-
-    override func draw(_ rect: CGRect) {
-
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-
-        context.beginPath()
-        context.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        context.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        context.addLine(to: CGPoint(x: (rect.maxX / 2.0), y: rect.minY))
-        context.closePath()
-
-        context.setFillColor(red: 1.0, green: 0.5, blue: 0.0, alpha: 0.60)
-        context.fillPath()
-    }
-    
-    enum Edge {
-        case topLeft
-        case topRight
-        case bottomRight
-        case bottomLeft
-        case none
-    }
-
-    var proxy: CGFloat = 30
-    var touchStart = CGPoint.zero
-    var edgeTouched: Edge = .none
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
         
-        self.backgroundColor = .red
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard State.current == .resize else { return }
+        
         if let touch = touches.first {
 
             touchStart = touch.location(in: self)
@@ -297,8 +314,10 @@ class ResizableView: UIView {
             }()
         }
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard State.current == .resize else { return }
+        
         if let touch = touches.first {
             let currentPoint = touch.location(in: self)
             let previous = touch.previousLocation(in: self)
@@ -311,7 +330,6 @@ class ResizableView: UIView {
             let deltaWidth = currentPoint.x - previous.x
             let deltaHeight = currentPoint.y - previous.y
 
-            print(edgeTouched)
             switch edgeTouched {
             case .topLeft:
                 frame = CGRect(x: originX + deltaWidth, y: originY + deltaHeight, width: width - deltaWidth, height: height - deltaHeight)
@@ -326,10 +344,60 @@ class ResizableView: UIView {
             }
         }
     }
-
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         edgeTouched = .none
     }
+    
+    @objc func detectPan(_ recognizer: UIPanGestureRecognizer) {
+        guard State.current == .move || State.current == .rotate else { return }
+        
+        let location = recognizer.location(in: self)
+        let gestureRotation = CGFloat(angle(location)) - startRotationAngle
+        let translation  = recognizer.translation(in: self.superview)
+        
+        if State.current == .move {
+            self.center = CGPoint(x: lastLocation.x + translation.x, y: lastLocation.y + translation.y)
+        } else if State.current == .rotate {
+            switch recognizer.state {
+            case .began:
+                startRotationAngle = angle(location)
+            case .changed:
+                rotate(to: rotation - gestureRotation.degreesToRadians)
+            case .ended:
+                rotation -= gestureRotation.degreesToRadians
+            default :
+                break
+            }
+        }
+    }
+    
+    func rotate(to value: CGFloat) {
+        rotateAnimation.fromValue = value
+        rotateAnimation.toValue = value
+        rotateAnimation.duration = 0
+        rotateAnimation.repeatCount = 0
+        rotateAnimation.isRemovedOnCompletion = false
+        rotateAnimation.fillMode = CAMediaTimingFillMode.forwards
+        rotateAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        layer.add(rotateAnimation, forKey: "transform.rotation.z")
+    }
+    
+    func angle(_ location: CGPoint) -> CGFloat {
+        let deltaY = location.y - center.y
+        let deltaX = location.x - center.x
+        let angle = atan2(deltaY, deltaX) * 180 / .pi
+        return angle < 0 ? abs(angle) : 360 - angle
+    }
+    
+    enum Edge {
+        case topLeft
+        case topRight
+        case bottomRight
+        case bottomLeft
+        case none
+    }
+    
 }
 
 class TriangleView : UIView {
@@ -359,64 +427,9 @@ class TriangleView : UIView {
     }
 }
 
-class RotateShape: UIView {
-    
-    var rotation: CGFloat = 0.0
-    var startRotationAngle: CGFloat = 0
-    let rotateAnimation = CABasicAnimation()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        let panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(detectPan))
-        gestureRecognizers = [panRecognizer]
-
-        backgroundColor = .red
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc func detectPan(_ recognizer: UIPanGestureRecognizer) {
-        let location = recognizer.location(in: self)
-        let gestureRotation = CGFloat(angle(location)) - startRotationAngle
-        
-        switch recognizer.state {
-        case .began:
-            startRotationAngle = angle(location)
-        case .changed:
-            rotate(to: rotation - gestureRotation.degreesToRadians)
-        case .ended:
-            rotation -= gestureRotation.degreesToRadians
-        default :
-            break
-        }
-    }
-    
-    func rotate(to value: CGFloat) {
-        rotateAnimation.fromValue = value
-        rotateAnimation.toValue = value
-        rotateAnimation.duration = 0
-        rotateAnimation.repeatCount = 0
-        rotateAnimation.isRemovedOnCompletion = false
-        rotateAnimation.fillMode = CAMediaTimingFillMode.forwards
-        rotateAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-        layer.add(rotateAnimation, forKey: "transform.rotation.z")
-    }
-    
-    func angle(_ location: CGPoint) -> CGFloat {
-        let deltaY = location.y - center.y
-        let deltaX = location.x - center.x
-        let angle = atan2(deltaY, deltaX) * 180 / .pi
-        return angle < 0 ? abs(angle) : 360 - angle
-    }
-}
-
 extension CGFloat {
     var degreesToRadians: Self { return self * .pi / 180 }
 }
-
 
 // Present the view controller in the Live View window
 let navigationController = UINavigationController(rootViewController: CanvasViewController())
